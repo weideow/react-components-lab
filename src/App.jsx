84d2cd6cc81b741e28c84752ef97bcc57262d927
weideow <owweide@hotmail.com>
2025-01-14 21:35:29 +0800

    init commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,10 @@
-
-import WeatherForecast from './WeatherForecast/WeatherForecast'; 
-import './WeatherForecast/WeatherForecast'; 
-
-const weatherForecasts = [
-  {
-    day: 'Mon',
-    img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-components-lab/assets/day.svg',
-    imgAlt: 'sun icon',
-    conditions: 'sunny',
-    time: 'Morning',
-  },
-  {
-    day: 'Tue',
-    img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-components-lab/assets/night.svg',
-    imgAlt: 'moon icon',
-    conditions: 'clear',
-    time: 'Night',
-  },
-  {
-    day: 'Wed',
-    img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-components-lab/assets/stormy.svg',
-    imgAlt: 'clouds with lightning icon',
-    conditions: 'stormy',
-    time: 'All Day',
-  },
-  {
-    day: 'Thu',
-    img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-components-lab/assets/cloudy-day.svg',
-    imgAlt: 'sun overcast by clouds icon',
-    conditions: 'overcast',
-    time: 'Evening',
-  },
-  {
-    day: 'Fri',
-    img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-components-lab/assets/cloudy-night.svg',
-    imgAlt: 'moon overcast by clouds icon',
-    conditions: 'cloudy',
-    time: 'Night',
-  },
-];
+// src/App.jsx
 
 const App = () => {
-return (
-<>
-<h1>Local Weather</h1>
-<section>
-  {weatherForecasts.map((forecast, index) => (
-    <WeatherForecast key={index}
-    <h2>{forecast.day}</h2>
-    <img src={img} alt={forecast.imgAlt} />
-    <p><span>conditions: </span>{forecast.conditions}</p>
-    <p><span>time: </span>{forecast.time}</p>
- />
-  ))};
 
+  return (
+    <h1>Hello world!</h1>
+  );
+}
 
-</section>
-
-</>
-
-);
-};
-
-export default App;
+export default App
